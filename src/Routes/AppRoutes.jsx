08a4cf464895408907b82534a616181d6256f2b8
FaresOps <fares.s@consultingdatamed.com>
2025-02-19 15:16:52 +0100

    added /deposer route

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,28 +1,19 @@
-import React from 'react'
-import { Routes } from 'react-router-dom'
-import ScrollToTop from '../Utils/ScrollToTop'
-import Home from '../pages/Home/Home'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Details from "../pages/Details/Details";
+// import ScrollToTop from "../Utils/ScrollToTop";
 
 const AppRoutes = () => {
   return (
     <div>
-    <ScrollToTop/>
+      {/* <ScrollToTop /> */}
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/esn/Accueil" element={<Home />} /> */}
-        {/* <Route path="/esn/carriere" element={<Career />} /> */}
-        {/* <Route path="/esn/carriere/:id" element={<CareerDetail />} /> */}
-        {/* <Route path="/esn/Actualités" element={<Blog />} /> */}
-        {/* <Route path="/esn/Actualités/:id" element={<BlogDetail />} /> */}
-        {/* <Route path="/esn/Expertise" element={<Expertise />} /> */}
-        {/* <Route path="/esn/Contact" element={<Contact />} /> */}
-        {/* <Route path="/politiquedeconfidentialite" element={<Policy />} /> */}
-        {/* <Route path="/Mentionslegales" element={<Mention />} /> */}
-        {/* <Route path="/politiquedecookies" element={<Cookies />} /> */}
-        {/* <Route path="/*" element={<NotFound />} /> */}
+        <Route path="/Deposer" element={<Details />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;

dff97b21646c5d56bf497b25fbb1d61951b80841
FaresOps <fares.s@consultingdatamed.com>
2025-02-19 15:47:03 +0100

    leftside static

--- src/Components/LeftSide.jsx
+++ src/Components/LeftSide.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
-const leftSide = () => {
+const leftSide = ({title,paragraphe}) => {
   return (
-    <div>leftSide</div>
-  )
-}
+    <div className="p-5 sm:pl-14 sm:w-1/2">
+      <h1 className="font-montserrat text-[40px] font-semibold leading-[120.402%] capitalize bg-gradient-to-r from-[#173A6D] to-[#2D70D3] bg-clip-text text-transparent">{title}</h1>
+      <p className="text-black font-montserrat text-[18px] font-normal leading-[120.402%]">{paragraphe}</p>
+    </div>
+  );
+};
 
-export default leftSide
+export default leftSide;

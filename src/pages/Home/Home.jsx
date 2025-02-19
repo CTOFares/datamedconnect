41c9d5c525e0ac4e68cd79c6974fb94e79e6c58b
FaresOps <fares.s@consultingdatamed.com>
2025-02-19 15:17:27 +0100

    Hom Cploeted

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../../assets/assets";
+import Hero from "../../Components/Home/Hero";
+import Consultant from "../../Components/Home/Consultant";
+import Clients from "../../Components/Home/Clients";
+import Footer from "../../Components/Footer";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <>
+      <Hero />
+      <Consultant/>
+      <Clients/>
+      <Footer/>
+    </>
+  );
+};
 
-export default Home
+export default Home;

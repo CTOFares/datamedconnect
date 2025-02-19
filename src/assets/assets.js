411ec86e34e8bc992d2d04f0f5f402c4571cfde7
FaresOps <fares.s@consultingdatamed.com>
2025-02-19 15:16:26 +0100

    added images

---
+++ src/assets/assets.js
@@ -0,0 +1,27 @@
+import logo from "./logo.png";
+import client1 from "./client1.png";
+import client2 from "./client2.png";
+import client3 from "./client3.png";
+import client4 from "./client4.png";
+import logowhite from "./logowhite.png";
+// Export assets
+export const assets = {
+  logo,
+  logowhite
+};
+
+
+export const carouselclient = {
+  client1,
+  client2,
+  client3,
+  client4,
+  client1,
+  client2,
+  client3,
+  client4,
+  client1,
+  client2,
+  client3,
+  client4,
+};

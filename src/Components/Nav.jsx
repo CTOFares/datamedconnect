8e6249ae5e203cb8d0f705f95045a20d44036557
FaresOps <fares.s@consultingdatamed.com>
2025-02-19 15:46:51 +0100

    Nav completed for the process

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 
 const Nav = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
-    <div className="w-full p-9">
-      <img src={assets.logo} alt="" />
+    <div className="w-full items-center justify-center sm:justify-start flex p-9">
+      <img
+        src={assets.logo}
+        alt="Logo"
+        onClick={handleClick}
+        className="cursor-pointer"
+      />
     </div>
   );
 };

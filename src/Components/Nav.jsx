6bb6f2e729eca2006ee8e045605b63459d573f17
FaresOps <fares.s@consultingdatamed.com>
2025-02-19 15:16:42 +0100

    Created nav for all the forms pages

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Nav = () => {
+  return (
+    <div className="w-full p-9">
+      <img src={assets.logo} alt="" />
+    </div>
+  );
+};
+
+export default Nav;

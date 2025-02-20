5fc1f3accb2396b69711869d4e1cbccf620e8bf2
FaresOps <fares.s@consultingdatamed.com>
2025-02-20 10:38:26 +0100

    Added Clients

--- src/Components/Home/Clients.jsx
+++ src/Components/Home/Clients.jsx
@@ -13,7 +13,7 @@ const Clients = () => {
   }, []);
 
   return (
-    <div className="sm:p-[100px] my-9 sm:my-0 px-[10px]"> 
+    <div className="sm:p-[100px] my-9 sm:my-0 px-[10px]">
       <div className="rounded-[15px] gap-6 sm:flex p-9  border ">
         <div className="w-full sm:w-1/5 border-[rgba(0,0,0,0.10)]">
           <p className="font-montserrat text-[20px] sm:text-[40px] font-semibold sm:leading-[48.161px] bg-gradient-to-r from-[#173A6D] to-[#2D70D3] bg-clip-text text-transparent">
@@ -27,19 +27,47 @@ const Clients = () => {
 
         <div className=" w-full sm:w-4/5 flex p-6 overflow-hidden">
           <div
-            className="flex transition-all duration-[30000ms] gap-9 " // Smooth transition on X-axis
+            className="flex transition-all duration-[30000ms] gap-11 " // Smooth transition on X-axis
             style={{
               transform: `translateX(${translateX}%)`, // Apply translation based on state
             }}
           >
-            <img src={carouselclient.client1} alt="client1" />
-            <img src={carouselclient.client2} alt="client2" />
-            <img src={carouselclient.client3} alt="client3" />
-            <img src={carouselclient.client4} alt="client4" />
-            <img src={carouselclient.client1} alt="client1" />
-            <img src={carouselclient.client2} alt="client2" />
-            <img src={carouselclient.client3} alt="client3" />
-            <img src={carouselclient.client4} alt="client4" />
+            <img src={carouselclient.client1} alt="client1" className="w-[200px] h-[90px]" />
+            <img
+              src={carouselclient.client2}
+              alt="client2"
+              className="w-[200px] h-[90px]"
+            />
+            <img src={carouselclient.client3} alt="client3"  className="w-[200px] h-[90px]"/>
+            <img
+              src={carouselclient.client4}
+              alt="client4"
+              className="w-full"
+            />
+            {/* <img src={carouselclient.client5} alt="client5" className="w-[200px] h-[90px]" /> */}
+            {/* <img src={carouselclient.client6} alt="client6" className="w-[200px] h-[90px]" /> */}
+            {/* <img src={carouselclient.client7} alt="client7" /> */}
+            <img src={carouselclient.client8} alt="client8" />
+            <img
+              src={carouselclient.client9}
+              alt="client9"
+              className="w-[200px] h-[90px]"
+            />
+            <img
+              src={carouselclient.client10}
+              alt="client10"
+              className="w-[200px] h-[90px]"
+            />
+            <img
+              src={carouselclient.client11}
+              alt="client11"
+              className="w-[200px] h-[90px]"
+            />
+            <img
+              src={carouselclient.client12}
+              alt="client12"
+              className="w-[200px] h-[90px]"
+            />
           </div>
         </div>
       </div>
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,8 +1,19 @@
 import logo from "./logo.png";
-import client1 from "./client1.png";
-import client2 from "./client2.png";
-import client3 from "./client3.png";
-import client4 from "./client4.png";
+import client1 from "./clients/client1.png";
+import client2 from "./clients/client2.png";
+import client3 from "./clients/client3.png";
+import client4 from "./clients/client4.png";
+// import client5 from "./clients/client5.png";
+// import client6 from "./clients/client6.png";
+// import client7 from "./clients/client7.png";
+import client8 from "./clients/client8.png";
+import client9 from "./clients/client9.png";
+import client10 from "./clients/client10.png";
+import client11 from "./clients/client11.png";
+import client12 from "./clients/client12.png";
+
+
+
 import logowhite from "./logowhite.png";
 import iconpdf from "./iconpdf.svg";
 import grid from "./grid.svg";
@@ -26,12 +37,12 @@ export const carouselclient = {
   client2,
   client3,
   client4,
-  client1,
-  client2,
-  client3,
-  client4,
-  client1,
-  client2,
-  client3,
-  client4,
+  // client5,
+  // client6,
+  // client7,
+  client8,
+  client9,
+  client10,
+  client11,
+  client12
 };

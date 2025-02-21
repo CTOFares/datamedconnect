d8bad73a6098fd0f2f792795511bdbeb75ad40be
FaresOps <fares.s@consultingdatamed.com>
2025-02-21 09:37:13 +0100

    Home responsive Completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,14 +5,20 @@ import Hero2 from "../../Components/Home/Home2";
 import Consultant from "../../Components/Home/Consultant";
 import Clients from "../../Components/Home/Clients";
 import Footer from "../../Components/Footer";
+import Hero3 from "../../Components/Home/Hero3";
+import Nav from "../../Components/Nav";
+import Testimonials from "../../Components/Home/Testimonials";
 
 const Home = () => {
   return (
     <>
-      <Hero2 />
+      <Nav />
+      <Hero3 />
+      {/* <Hero2 /> */}
       {/* <Hero /> */}
       <Clients />
       <Consultant />
+      <Testimonials/>
       <Footer />
     </>
   );

e0f1d96bd4c85a64f8a76378d2d2009894ea37d7
FaresOps <fares.s@consultingdatamed.com>
2025-02-21 16:54:31 +0100

    Removed Company

--- src/Components/Home/Clients.jsx
+++ src/Components/Home/Clients.jsx
@@ -33,11 +33,11 @@ const Clients = () => {
             }}
           >
             <img src={carouselclient.client1} alt="client1" className="w-[200px] h-[90px] mx-5" />
-            <img
+            {/* <img
               src={carouselclient.client2}
               alt="client2"
               className="w-[200px] h-[90px]"
-            />
+            /> */}
             <img src={carouselclient.client3} alt="client3"  className="w-[200px] h-[90px]  mx-5"/>
             <img
               src={carouselclient.client4}

c179c0dcbed0a63faff89cdcbefe642548833f7f
FaresOps <fares.s@consultingdatamed.com>
2025-03-24 15:38:49 +0100

    Remouved Versel Insights

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import "./App.css";
 import Constraction from "./Components/Constraction";
 import AppRoutes from "./Routes/AppRoutes";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +9,6 @@ function App() {
   return (
     <>
       <AppRoutes />
-      <SpeedInsights />
     </>
   );
 }

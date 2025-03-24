f64b42873e16819c42ddee4d7e85cbf05aa09812
FaresOps <fares.s@consultingdatamed.com>
2025-03-24 15:35:48 +0100

    hello vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import Constraction from './Components/Constraction'
-import AppRoutes from './Routes/AppRoutes'
+import { useState } from "react";
+import "./App.css";
+import Constraction from "./Components/Constraction";
+import AppRoutes from "./Routes/AppRoutes";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <AppRoutes/>
+      <AppRoutes />
+      <SpeedInsights />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
